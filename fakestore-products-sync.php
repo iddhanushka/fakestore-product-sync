fbf09ee8c43a5702653172f31c71f84279e0399d
Dhanushka <diddamaldeniya@gmail.com>
2025-09-15 03:07:34 +0530

    developed Fakestore admin setting poge

--- fakestore-products-sync.php
+++ fakestore-products-sync.php
@@ -8,9 +8,48 @@
 
   if ( ! defined( 'ABSPATH' ) ) exit;
 
-  add_filter('the_content', 'addToEndOfPost'); 
+  class FakestoreProductSyncPlugin {
+    function __construct() {
+      add_action('admin_menu', array($this, 'adminPage'));
+      add_action('admin_init', array($this, 'settings'));
+    }
 
-  function addToEndOfPost($content) { 
-    return $content . '<p>My name is Dhanushka.</p>';
+    function settings() {
+      add_settings_section('fsp_first_section', null, null, 'fakestore-settings-page');
+      add_settings_field('fsp_api_url', 'API Base URL', array($this, 'apiHTML'), 'fakestore-settings-page', 'fsp_first_section');
+      register_setting('fakestoreplugin', 'fsp_api_url', array('sanitize_callback' => array($this, 'sanitizeAPIURL'), 'default' => 'https://fakestoreapi.com/products')); 
+    }
+
+    function sanitizeAPIURL($input) {
+      if (empty($input)) {
+        add_settings_error('fsp_api_url', 'fsp_apiurl_error', 'API URL must be entered.');
+        return get_option('fsp_api_url');
+      }
+
+      return $input;
+    }
+
+    function apiHTML() { ?>
+      <input type="url" name="fsp_api_url" value="<?php echo esc_attr(get_option('fsp_api_url', 'https://fakestoreapi.com/products')); ?>" size="60">
+    <?php }
+
+    function adminPage() {
+      add_options_page('Fakestore Settings', 'Fakestore', 'manage_options', 'fakestore-settings-page', array($this, 'pageHTML'));
+    }
+
+    function pageHTML() { ?>
+      <div class="wrap">
+        <h1>Fakestore Settings</h1>
+        <form action="options.php" method="POST">
+          <?php 
+            settings_fields('fakestoreplugin');
+            do_settings_sections('fakestore-settings-page');
+            submit_button(); 
+          ?>
+        </form>
+      </div>
+    <?php }
   }
+
+  $fakestoreProductSyncPlugin = new FakestoreProductSyncPlugin;
 ?>
